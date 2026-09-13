167c9c58e3533081c559c93c412283a1bd3c624a
Nitto26 <nittojoshy1234@gmail.com>
2026-09-13 22:42:47 +0530

    chore(release): bump app version to v2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.geoalarm"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.1"
+        versionCode = 3
+        versionName = "2.2"
 
         // --- THE VAULT READER ---
         val keystoreProperties = Properties()
